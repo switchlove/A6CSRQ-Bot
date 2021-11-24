37b179a6c9a1f182e5c584cc5081245124fa8b86
Switch <switchbladelove@gmail.com>
2021-11-24 15:37:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -539,7 +539,7 @@
 				}
 				console.log(roleArr);
 				for( var i = 0; i < roleArr.length; i++){ 
-					let role = message.guild.roles.find(r => r.name === roleArr[i]);
+					let role = message.guild.roles.cache.find(r => r.name === roleArr[i]);
 					role.setPermissions([Permissions.FLAGS.VIEW_CHANNELS]);
 				}
 			}
